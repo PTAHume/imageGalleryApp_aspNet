4a0b7f4cf5729ada153955791ca76d6d91540b6a
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-07-24 17:29:32 +0300

    first commit

---
+++ image_gallery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace image_gallery.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
